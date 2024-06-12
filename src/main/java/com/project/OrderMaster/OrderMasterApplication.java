8fc3bb8387f95da2b2722b2e4b4850ea2e1323ba
Pamela <pamela_borges18@hotmail.com>
2024-06-12 12:23:36 -0300

    Init the project

---
+++ src/main/java/com/project/OrderMaster/OrderMasterApplication.java
@@ -0,0 +1,13 @@
+package com.project.OrderMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/OrderMaster/OrderMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.OrderMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

53fc481f26e596b353340a69568670f6f9e41981
Pamela <pamela_borges18@hotmail.com>
2024-06-12 13:58:29 -0300

    feat(Controller): Criação do controller e implementação dos Endpoints

---
+++ src/main/java/com/project/OrderMaster/controller/productController.java
@@ -0,0 +1,68 @@
+package com.project.OrderMaster.controller;
+
+import com.project.OrderMaster.entity.Product;
+import com.project.OrderMaster.service.productService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/product")
+public class productController {
+
+    @Autowired
+    private productService service;
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        try {
+            Product newProduct = service.create(product);
+            return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+    }
+
+    @PutMapping("{/id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+        try {
+            Product updateProduct = service.update(id, product);
+            return ResponseEntity.ok().body(updateProduct);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll() {
+        try {
+            List<Product> list = service.getAll();
+            return ResponseEntity.ok().body(list);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getById(@PathVariable Long id) {
+        Optional<Product> prodOptional = service.getById(id);
+        if (prodOptional.isPresent()) {
+            return ResponseEntity.ok().body(prodOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}

8ec2ae1923c94f8cebdf13ecd495dd4413c06d1b
Pamela <pamela_borges18@hotmail.com>
2024-06-13 17:56:32 -0300

    feat(Docker): Containerização com Docker

--- src/main/java/com/project/OrderMaster/security/SecurityFilter.java
+++ src/main/java/com/project/OrderMaster/security/SecurityFilter.java
@@ -10,10 +10,12 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     TokenService tokenService;
